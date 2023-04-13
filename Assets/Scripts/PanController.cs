4f8e73125247a20607e80f3f77fb47862a2c8288
byseinbolt <busovitor@gmail.com>
2023-04-13 22:10:01 +0300

    init

--- Assets/Scripts/PanController.cs
+++ Assets/Scripts/PanController.cs
@@ -3,28 +3,7 @@ using UnityEngine.UI;
 
 public class PanController : MonoBehaviour
 {
-    //todo в дальнейшем удалить с этого класса.тест 
-    [SerializeField] private Button _meat;
-    
-    [SerializeField] private PanView _panView;
-    private PanModel _panModel;
-
-    private void Awake()
-    {
-        _meat.onClick.AddListener(StartCooking);
-    }
-
-    public void Initialize(PanModel panModel)
-    {
-        _panModel = panModel;
-    }
-
-    private void StartCooking()
-    {
-        if (_panModel.IsEmpty)
-        {
-            _panView.CookMeat(_panModel.CookingTime);
-            _panModel.IsEmpty = false;
-        }
-    }
+    [SerializeField] private bool _isAvailable;
+    [SerializeField] private float _cookingTime;
 }
+
